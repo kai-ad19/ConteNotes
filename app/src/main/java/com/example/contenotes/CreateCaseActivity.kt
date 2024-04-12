12a656845acfea9bdda847ce7752caaaf804ec7a
Kai <kaidart@hotmail.com>
2024-04-12 19:34:41 +0100

    Cases displayed

--- app/src/main/java/com/example/contenotes/CreateCaseActivity.kt
+++ app/src/main/java/com/example/contenotes/CreateCaseActivity.kt
@@ -1,10 +1,15 @@
 package com.example.contenotes
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputFilter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-
+import kotlinx.coroutines.handleCoroutineException
+import android.graphics.Color
+import android.widget.Toast
 
 class CreateCaseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,16 +28,50 @@ class CreateCaseActivity : ComponentActivity() {
             finish()
         }
 
+        // 'Page setup' - Assign header text, return and continue button titles.
         headerText.text = "Creating New Case"
         returnButton.text = "Back"
         continueButton.text = "Finish"
 
 
+        // Text Fields
+        val edit_caseTitle: EditText = findViewById(R.id.editText_caseTitle)
+        val edit_caseDescription: EditText = findViewById(R.id.editText_caseDescription)
+        val edit_caseColour: EditText = findViewById(R.id.editText_caseColour)
+        val frame_colourTag: FrameLayout = findViewById(R.id.colourtag)
+        var input_hexcode = "000000"
+
+        // Update 'colour tag' when the hex code editor loses focus
+        edit_caseColour.setOnFocusChangeListener{ _, hasFocus ->
+            if(!hasFocus){
+                input_hexcode = edit_caseColour.text.toString().trim()
+                if (input_hexcode.length < 6){
+                    input_hexcode = input_hexcode.padEnd(6, '0') // Replace missing values with 0
+                }
+                frame_colourTag.setBackgroundColor(android.graphics.Color.parseColor("#$input_hexcode"))
+            }
+        }
 
 
         continueButton.setOnClickListener{
-            val intent = Intent(this, ListCaseActivity::class.java)
-            startActivity(intent)
+            // get values
+            val input_title = edit_caseTitle.text.toString().trim()
+            val input_description = edit_caseDescription.text.toString().trim()
+            input_hexcode = edit_caseColour.text.toString().trim()
+            input_hexcode = input_hexcode.padEnd(6, '0')
+
+            // verify that title and description have been provided
+            if (input_title.isNotEmpty() && input_description.isNotEmpty()){
+                val dbHelper = SQLDatabaseActivity(this)
+                dbHelper.newCase(input_title, input_description, input_hexcode)
+
+                val intent = Intent(this, ListCaseActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Missing case Title or Description", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/example/contenotes/ListCaseActivity.kt
+++ app/src/main/java/com/example/contenotes/ListCaseActivity.kt
@@ -6,35 +6,57 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import android.database.sqlite.SQLiteDatabase
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import org.w3c.dom.Text
 
 
 class ListCaseActivity : ComponentActivity() {
     private lateinit var dbHelper: SQLDatabaseActivity // Initiate database for getting cases
     @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_caselist)
+
+        val caseListContainer : LinearLayout = findViewById(R.id.caselistcontainer)
+
         dbHelper = SQLDatabaseActivity(this)
-        println("INITIATING DATABASE")
         val cursor = dbHelper.fetchAllCases()
-        println(cursor)
         if (cursor != null){ // There are cases
             while (cursor.moveToNext()) { // Iterate through each record in the table
+                // Get record values being used
                 val name = cursor.getString(cursor.getColumnIndex("name"))
                 val accessed = cursor.getString(cursor.getColumnIndex("lastAccessed"))
                 val colour = cursor.getString(cursor.getColumnIndex("colour"))
                 val id = cursor.getInt(cursor.getColumnIndex("id"))
 
-                println("CASE")
-                println(id)
+                println("CASE CONTENTS")
                 println(name)
                 println(accessed)
                 println(colour)
+                println(id)
+
+                // Generate new case frame
+                val outerCaseFrame = LayoutInflater.from(this).inflate(R.layout.caseframe, null) as LinearLayout
+                val innerCaseFrame = outerCaseFrame.findViewById<FrameLayout>(R.id.caseframe)
+                val caseXML_Id = innerCaseFrame.findViewById<TextView>(R.id.caseid)
+                val caseXML_Title = innerCaseFrame.findViewById<TextView>(R.id.casetitle)
+                val caseXML_Accessed = innerCaseFrame.findViewById<TextView>(R.id.caseaccessed)
+                val caseXML_Colour = innerCaseFrame.findViewById<FrameLayout>(R.id.colourtag)
+
+                // Assign values
+                val paddedId = id.toString().padStart(3, '0')
+                caseXML_Id.text = String.format("Case #%03d", id)
+                caseXML_Title.text = name
+                caseXML_Accessed.text = accessed
+                caseXML_Colour.setBackgroundColor(android.graphics.Color.parseColor("#$colour"))
+
+                caseListContainer.addView(outerCaseFrame)
             }
         }
 
         println("COMPLETED - LOADING PAGE")
-
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.page_caselist)
 
 
         val headerText: TextView = findViewById(R.id.header_text)
